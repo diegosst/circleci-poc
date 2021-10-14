abf8badbc3b614f545f0e1c44e7a0f68a0aa32e9
Diego Santos <diego.santos@keyrus.com.br>
2021-10-14 14:52:27 -0300

    Initial commit with basic springboot application.

---
+++ src/test/java/br/com/dss/circleci/CircleciApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dss.circleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
